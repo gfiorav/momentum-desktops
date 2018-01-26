da4efb829fe041251b5f649cf6ec274f97e6b707
Guido Fioravantti <guido.fioravantti@gmail.com>
2018-01-26 20:27:46 +0100

    Makeup

--- extension.js
+++ extension.js
@@ -1,58 +1,49 @@
 
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
 const Util = imports.misc.util;
 
-let text, button;
+let button;
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
+/**
+ * Initializes the extension
+ */
+function init() { // eslint-disable-line no-unused-vars
+    button = new St.Bin({
+        style_class: 'panel-button',
+        reactive: true,
+        can_focus: true,
+        x_fill: true,
+        y_fill: false,
+        track_hover: true,
+    });
 
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
-}
-
-function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
+    let icon = new St.Icon({
+        icon_name: 'system-run-symbolic',
+        style_class: 'system-status-icon',
+    });
 
     button.set_child(icon);
     button.connect('button-press-event', changeWallpaper);
 }
 
-function enable() {
+/**
+ * Enables extension
+ */
+function enable() { // eslint-disable-line no-unused-vars
     Main.panel._rightBox.insert_child_at_index(button, 0);
 }
 
-function disable() {
+/**
+ * Disables extension
+ */
+function disable() { // eslint-disable-line no-unused-vars
     Main.panel._rightBox.remove_child(button);
 }
 
+/**
+ * Changes wallpaper
+ */
 function changeWallpaper() {
     Util.spawn(
         [
@@ -60,7 +51,14 @@ function changeWallpaper() {
             'set',
             'org.gnome.desktop.background',
             'picture-uri',
-            'file:///home/guido/Pictures/backgrounds/background.jpg'
-        ])
+            'file:///home/guido/Pictures/backgrounds/background.jpg',
+        ]);
+}
+
+/**
+ * Creates a unique token to encrypt/decrypt user credentials for storage
+ */
+function initializeSecret() {
+  
 }
 
